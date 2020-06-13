a1b0174cb84eb4db91df53d9947fe534b5512676
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-06-13 22:05:04 +0100

    Video schema

---
+++ server/models/Video.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema
+
+//The writer has a type of schema.types b/c we want to reference a user,
+//and bring in all the details of that user.
+const videoSchema = mongoose.Schema({
+  writer: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  title: {
+    type: String,
+    maxlength: 50,
+  },
+  description: {
+    type: String,
+  },
+  privacy: {
+    type: Number,
+  },
+  filePath: {
+    type: String,
+  },
+  catogory: String,
+  views: {
+    type: Number,
+    default: 0
+  },
+  duration: {
+    type: String
+  },
+  thumbnail: {
+    type: String
+  }
+}, { timestamps: true })
+
+//timestap will automatically generate the createdAt and updatedAt
+
+const Video = mongoose.model('Video', videoSchema)
+
+module.exports = { Video }
