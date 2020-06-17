b894247f5c6d259972a6ccb3c8cfd8298622f006
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-06-17 17:43:43 +0100

    Subscribe feature 2

--- client/src/components/views/DetailVideoPage/Sections/Subscriber.js
+++ client/src/components/views/DetailVideoPage/Sections/Subscriber.js
@@ -4,12 +4,45 @@ import axios from 'axios'
 
 const Subscriber = ({ userTo, userFrom }) => {
 
-  const subscribeNumberVariables = { userTo, userFrom }
-
   const [SubscribeNumber, setSubscribeNumber] = useState(0)
   const [Subscribed, setSubscribed] = useState(false)
 
+
+  const subscribeNumberVariables = { userTo, userFrom }
+
+  const onSubscribe = () => {
+   
+    let subscribeVariables = { userTo, userFrom }
+
+   if (Subscribed) {
+     //when we're already subscribed
+     axios.post('/api/subscribe/unSubscribe', subscribeVariables)
+     .then(response => {
+       if (response.data.success) {
+        setSubscribeNumber(SubscribeNumber - 1)
+        setSubscribed(!Subscribed)
+       } else {
+         alert('Failed to unsubscribe')
+       }
+     })
+   } 
+   else {
+     //when we're not subscribed
+     axios.post('/api/subscribe/subscribe', subscribeVariables)
+     .then(response => {
+       if (response.data.success) {
+        setSubscribeNumber(SubscribeNumber + 1)
+        setSubscribed(!Subscribed)
+       } else {
+         alert('Failed to subscribe')
+       }
+     })
+   }
+  }
+
   useEffect(() => {
+  
+
     axios.post('/api/subscribe/subscribeNumber', subscribeNumberVariables)
       .then(response => {
         if (response.data.success) {
@@ -20,29 +53,32 @@ const Subscriber = ({ userTo, userFrom }) => {
           alert('Failed to get subscriber number')
         }
       })
-
+  
       //check if already subscribed
       axios.post('/api/subscribe/subscribed', subscribeNumberVariables)
       .then(response => {
         if (response.data.success) {
           // console.log(response.data.subscribed)
-          setSubscribed(response.data.subcribed)
+          setSubscribed(response.data.subscribed)
         } else {
           alert('Failed to get subscriber info')
         }
       })
 
-  }, [])
+  }, [subscribeNumberVariables])
+
+ 
 
   return (
     <div>
-      <button style={{
-        backgroundColor:`${Subscribed ? '#AAAAA' : '#CC0000'}`,
+      <button 
+      onClick={onSubscribe}
+      style={{
+        backgroundColor:`${Subscribed ? '#AAAAAA' : '#CC0000'}`,
         borderRadius: '3px', color: 'white',
         padding: '10px 16px', fontWeight: '500', fontSize: '1rem', textTransform: 'uppercase',
       }}>
-      {SubscribeNumber} {Subscribed ? 'Subscribed' :
-      'Subscribe'}
+      {SubscribeNumber} {Subscribed ? 'Subscribed' : 'Subscribe'}
    </button>
     </div>
   )
--- server/routes/subscribe.js
+++ server/routes/subscribe.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const { Subscriber } = require("../models/Subscriber")
 
+//number of subscribers
 router.post("/subscribeNumber", (req, res) => {
 
   Subscriber.find({ "userTo": req.body.userTo })
@@ -14,6 +15,7 @@ router.post("/subscribeNumber", (req, res) => {
 
 })
 
+//route for checking if u're a subscriber
 router.post("/subscribed", (req, res) => {
 
   Subscriber.find({ "userTo": req.body.userTo , "userFrom": req.body.userFrom })
@@ -30,6 +32,25 @@ router.post("/subscribed", (req, res) => {
       res.status(200).json({ success: true, subscribed: result  })
   })
 
-});
+})
+
+//subscribe route
+router.post("/subscribe", (req, res) => {
+  const subscribe = new Subscriber(req.body)
+  subscribe.save((err, doc) => {
+    if (err) return res.json({ subscribe: false, err })
+   return res.status(200).json({ success: true })
+  })
+})
+
+// unsubscribe
+router.post("/unSubscribe", (req, res) => {
+  Subscriber.findOneAndDelete({ userTo: req.body.userTo, userFrom: req.body.userFrom })
+    .exec((err, doc) => {
+      if (err) return res.status(400).json({ success: false, err })
+      res.status(200).json({ success: true, doc })
+    })
+})
 
 module.exports = router
+
